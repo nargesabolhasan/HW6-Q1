437dc1d1de1312dbf7ad895a8d7c5aedfd200f3b
narges Hajabolhasan <narges.abolhasan@gmail.com>
2021-12-14 22:25:03 +0330

    compare the arrays with indexOf

--- main.js
+++ main.js
@@ -3,5 +3,7 @@ function commonChar(s1, s2) {
 
     const arr1 = s1.split('');
     const arr2 = s2.split('');
+    for (let i = 0; i < arr1.length; i++) {
+        let index = arr2.indexOf(arr1[i]);
 
   
