50fcad63aff650d4b1f061dfc8077e40b1a1db79
narges Hajabolhasan <narges.abolhasan@gmail.com>
2021-12-14 22:25:51 +0330

    the end

--- main.js
+++ main.js
@@ -5,5 +5,15 @@ function commonChar(s1, s2) {
     const arr2 = s2.split('');
     for (let i = 0; i < arr1.length; i++) {
         let index = arr2.indexOf(arr1[i]);
+        if (index !== -1) {
+            arr2.splice(index, 1);
+            counter += 1
+        }
+    }
+
+   console.log(counter)
+
+} 
+commonChar("aabcc", "adcaa")
 
   
